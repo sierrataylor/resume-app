0c2a56ad8709519ae259fd5995f058bfdc873b49
sierrataylor <staylor23@jcu.edu>
2022-05-05 20:43:14 -0400

    Make style changes to UI

--- src/SubmitButton.tsx
+++ src/SubmitButton.tsx
@@ -1,8 +1,9 @@
+import React from "react";
 import { Button } from "react-bootstrap";
 
 export const FileSubmitButton = () => {
     return(
-        <Button variant="primary">
+        <Button variant="info" size="lg">
             Submit
         </Button>
     );
